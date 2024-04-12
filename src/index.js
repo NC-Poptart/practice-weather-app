d0e147f0d697462414a71c6c5261490137781df6
NC-Poptart <nsovo.c1@gmail.com>
2024-04-12 19:06:18 +0200

    added functionality to the project

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ function cityElement(event) {
   let display = document.querySelector("#city-display");
 
   display.innerHTML = searchInput.value;
+
+  let apiKey = "8005c0f1346d7o3334btcffa67aabd4b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 let searchButton = document.querySelector("#app-button");
 searchButton.addEventListener("click", cityElement);
+
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city-display");
+  cityElement.innerHTML = response.data.city;
+
+  let temperature = document.querySelector("#app-temperature");
+  let roundedTemperature = Math.round(response.data.temperature.current);
+
+  temperature.innerHTML = roundedTemperature;
+}
