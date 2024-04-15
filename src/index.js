b01c8af210b51dddc913653d15275bb7dcff311f
NC-Poptart <nsovo.c1@gmail.com>
2024-04-16 01:33:47 +0200

    added JS search functionality

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-function cityElement(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#city-input");
-  let display = document.querySelector("#city-display");
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
 
-  display.innerHTML = searchInput.value;
-
-  let apiKey = "8005c0f1346d7o3334btcffa67aabd4b";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayTemperature);
-}
-
-let searchButton = document.querySelector("#app-button");
-searchButton.addEventListener("click", cityElement);
-
-function displayTemperature(response) {
-  let cityElement = document.querySelector("#city-display");
   cityElement.innerHTML = response.data.city;
-
-  let temperature = document.querySelector("#app-temperature");
-  let roundedTemperature = Math.round(response.data.temperature.current);
-
-  temperature.innerHTML = roundedTemperature;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
+
+function searchCity(city) {
+  let apiKey = "8005c0f1346d7o3334btcffa67aabd4b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
